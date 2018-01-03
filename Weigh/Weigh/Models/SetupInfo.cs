afabcbeab3427deccedff19ba1c4ca30eb314e51
Evan Coleman <ecoleman2@gmail.com>
2018-01-02 22:14:25 -0700

    Validation confirmed with weight, max length enforcing behavior implemented

--- Weigh/Weigh/Models/SetupInfo.cs
+++ Weigh/Weigh/Models/SetupInfo.cs
@@ -12,15 +12,11 @@ namespace Weigh.Models
         private string _weight;
         [Required(ErrorMessage = "This field is required")]
         [StringLength(4, MinimumLength = 2, ErrorMessage = "Minimum 2 chars, max 4 chars")]
+        [CustomValidation(typeof(SetupInfoValidation), "WeightValidation")]
         public string Weight
         {
             get { return _weight; }
             set { SetProperty(ref _weight, value); }
-        }
-
-        private void WeightValidator()
-        {
-
         }
     }
 }
---
+++ Weigh/Weigh/Validation/SetupInfoValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Weigh.Validation
+{
+    public class SetupInfoValidation
+    {
+        public static ValidationResult WeightValidation(string weight)
+        {
+            bool isValid = true;
+            double Weight;
+
+            // Perform validation logic here and set isValid to true or false.
+            Weight = Convert.ToDouble(weight);
+
+            if (Weight < 50 || Weight > 800)
+            {
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(
+                    "Weight must be in the range (50-800)");
+            }
+        }
+
+
+    }
+}
