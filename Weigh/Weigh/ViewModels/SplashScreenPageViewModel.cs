9022ced8b17e68a00d6697820996c52b6aaaf2c7
Evan Coleman <ecoleman2@gmail.com>
2018-01-25 23:56:45 -0700

    Added splashscreen, need to move loading to take advantage of it

---
+++ Weigh/Weigh/ViewModels/SplashScreenPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Prism.Navigation;
+using Prism.Services;
+using Weigh.Helpers;
+
+namespace Weigh.ViewModels
+{
+	public class SplashScreenPageViewModel : ViewModelBase
+	{
+	    public SplashScreenPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService, IDeviceService deviceService)
+	        : base(navigationService)
+	    {
+	        _navigationService = navigationService;
+	    }
+
+	    private INavigationService _navigationService;
+
+	    public override async void OnNavigatedTo(NavigationParameters parameters)
+	    {
+            // TODO: Implement any initialization logic you need here. Example would be to handle automatic user login
+
+            // Simulated long running task. You should remove this in your app.
+	        //await Task.Delay(4000);
+
+            // After performing the long running task we perform an absolute Navigation to remove the SplashScreen from
+            // the Navigation Stack.
+            if (Settings.FirstUse == "yes")
+	            await NavigationService.NavigateAsync("/InitialSetupPage");
+	        else
+	            await NavigationService.NavigateAsync("/NavigationPage/NavigatingAwareTabbedPage");
+        }
+    }
+}
